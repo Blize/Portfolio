dd389d56d7667c2dc926020aeb61f90827608e6e
Matthias Mantsch <matthiasmantsch@MacBook-Pro.local>
2022-08-24 20:52:16 +0200

    main/clean(remove): remove not used code on abot and change color and shadow

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,338 +1,305 @@
 <template>
-  <div class="about">
-    <div class="fly-in-text" :class="{ hidden: isNotActive }">
-      <h1>A</h1>
-      <h1>B</h1>
-      <h1>O</h1>
-      <h1>U</h1>
-      <h1>T</h1>
-    </div>
+	<div class="about">
+		<div class="fly-in-text" :class="{ hidden: isNotActive }">
+			<h1>A</h1>
+			<h1>B</h1>
+			<h1>O</h1>
+			<h1>U</h1>
+			<h1>T</h1>
+		</div>
 
-    <div class="info-text">
-      <h4>
-        I am a 17 year old programmer and designer. I am at my second year at
-        swisscom. I am an application developer, and have experienced many areas
-        of computer science. Below here you can see my hobbies and more.
-      </h4>
-    </div>
+		<div class="info-text">
+			<h4>
+				I am a 17 year old programmer and designer. I am at my second year at swisscom. I am an application developer,
+				and have experienced many areas of computer science. Below here you can see my hobbies and more.
+			</h4>
+		</div>
 
-    <div class="hobby">
-      <div class="title-hobby">
-        <h1>Hobbies</h1>
-      </div>
-      <div class="hobby-object-parent">
-        <div
-          class="hobby-object-under-parent"
-          v-for="hobby in hobbys"
-          :key="hobby.hobbyTitle"
-        >
-          <span class="hobby-object-child" @click="openModal(hobby.id)">
-            <h3>{{ hobby.hobbyTitle }}</h3>
-          </span>
-          <div class="modal">
-            <div class="modal-content">
-              <span class="close" @click="closeModal()">&times;</span>
-              <h3>{{ modalText }}</h3>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+		<div class="hobby">
+			<div class="title-hobby">
+				<h1>Hobbies</h1>
+			</div>
+			<div class="hobby-object-parent">
+				<div class="hobby-object-under-parent" v-for="hobby in hobbys" :key="hobby.hobbyTitle">
+					<span class="hobby-object-child" @click="openModal(hobby.id)">
+						<h3>{{ hobby.hobbyTitle }}</h3>
+					</span>
+					<div class="modal">
+						<div class="modal-content">
+							<span class="close" @click="closeModal()">&times;</span>
+							<h3>{{ modalText }}</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-    <div class="projects-object-parent">
-      <div class="project-text">
-        <h1>Projects</h1>
-        <h4>Here is a small overview from my projects</h4>
-      </div>
+		<div class="projects-object-parent">
+			<div class="project-text">
+				<h1>Projects</h1>
+				<h4>Here is a small overview from my projects</h4>
+			</div>
 
-      <div
-        class="flip-card"
-        v-for="project in projects"
-        :key="project.projectTitle"
-      >
-        <div class="flip-card-inner">
-          <div class="flip-card-front">
-            <h2>{{ project.projectTitle }}</h2>
-            <h4>{{ project.projectInfo }}</h4>
-          </div>
-          <div class="flip-card-back">
-            <h1>{{ project.projectTitle }}</h1>
-            <p>{{ project.projectURL }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+			<div class="flip-card" v-for="project in projects" :key="project.projectTitle">
+				<div class="flip-card-inner">
+					<div class="flip-card-front">
+						<h2>{{ project.projectTitle }}</h2>
+						<h4>{{ project.projectInfo }}</h4>
+					</div>
+					<div class="flip-card-back">
+						<h1>{{ project.projectTitle }}</h1>
+						<p>{{ project.projectURL }}</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      isNotActive: true,
-      showIFrame: true,
-      windowWidth: 10,
-      hobbys: [
-        { id: 1, hobbyTitle: "Downhiller", hobbyModal: "WIP 1" },
-        { id: 2, hobbyTitle: "Programmer", hobbyModal: "WIP 2" },
-        { id: 3, hobbyTitle: "Gamer", hobbyModal: "WIP 3" },
-        { id: 4, hobbyTitle: "Drawer", hobbyModal: "WIP 4" },
-      ],
+	data() {
+		return {
+			isNotActive: true,
+			showIFrame: true,
+			windowWidth: 10,
+			hobbys: [
+				{ id: 1, hobbyTitle: 'Downhiller', hobbyModal: 'WIP 1' },
+				{ id: 2, hobbyTitle: 'Programmer', hobbyModal: 'WIP 2' },
+				{ id: 3, hobbyTitle: 'Gamer', hobbyModal: 'WIP 3' },
+				{ id: 4, hobbyTitle: 'Drawer', hobbyModal: 'WIP 4' },
+			],
 
-      modalText: "",
-      projects: [
-        {
-          projectTitle: "Portfolio",
-          projectInfo:
-            "The Portfolio is a little webapplication built by myself. This webapplication has only a frontend.",
-          projectURL: "https://blize.vercel.app",
-        },
-        {
-          projectTitle: "FaceApp",
-          projectInfo:
-            "This is a little application where I use a AI to recognize the emotion of the person who is looking in the camera.",
-          projectURL: "https://face-app-detector.vercel.app",
-        },
-        {
-          projectTitle: "Reddit-Bot",
-          projectInfo:
-            "This is a small Bot build with Python in school with some friends. You can send messages directly via Reddit.",
-          projectURL: "https://blize.vercel.app/src/code",
-        },
-      ],
-    };
-  },
-  mounted() {
-    setTimeout(() => {
-      this.isNotActive = false;
-    }, 250);
-  },
-  created() {
-    this.windowWidth = window.innerWidth;
-    this.$nextTick(() => {
-      window.addEventListener("resize", this.onResize);
-    });
-  },
-  watch: {
-    windowWidth(newWidth, oldWidth) {
-      this.windowWidth = newWidth;
-    },
-  },
-  methods: {
-    onResize() {
-      this.windowWidth = window.innerWidth;
-    },
-    testWindowWidth() {
-      if (this.windowWidth < 768) {
-        console.log("Your Window is to small to use this feature");
-        alert(
-          "Your current window-width(" +
-            this.windowWidth +
-            ") is to small to use this feature"
-        );
-      } else {
-        this.showIFrame = false;
-        setInterval(() => {
-          this.showIFrame = true;
-        }, 100000);
-      }
-    },
-    openModal(id) {
-      let myHobby = this.hobbys.filter((hobby) => hobby.id === id)[0];
-      this.modalText = myHobby.hobbyModal;
-      let modal = document.getElementsByClassName("modal")[0];
-      modal.style.display = "block";
-    },
-    closeModal() {
-      let modal = document.getElementsByClassName("modal")[0];
-      modal.style.display = "none";
-    },
-  },
-  beforeDestroy() {
-    window.removeEventListener("resize", this.onResize);
-  },
-};
+			modalText: '',
+			projects: [
+				{
+					projectTitle: 'Portfolio',
+					projectInfo:
+						'The Portfolio is a little webapplication built by myself. This webapplication has only a frontend.',
+					projectURL: 'https://blize.vercel.app',
+				},
+				{
+					projectTitle: 'FaceApp',
+					projectInfo:
+						'This is a little application where I use a AI to recognize the emotion of the person who is looking in the camera.',
+					projectURL: 'https://face-app-detector.vercel.app',
+				},
+				{
+					projectTitle: 'Reddit-Bot',
+					projectInfo:
+						'This is a small Bot build with Python in school with some friends. You can send messages directly via Reddit.',
+					projectURL: 'https://blize.vercel.app/src/code',
+				},
+			],
+		}
+	},
+	mounted() {
+		setTimeout(() => {
+			this.isNotActive = false
+		}, 250)
+	},
+	created() {
+		this.windowWidth = window.innerWidth
+		this.$nextTick(() => {
+			window.addEventListener('resize', this.onResize)
+		})
+	},
+	methods: {
+		openModal(id) {
+			let myHobby = this.hobbys.filter((hobby) => hobby.id === id)[0]
+			this.modalText = myHobby.hobbyModal
+			let modal = document.getElementsByClassName('modal')[0]
+			modal.style.display = 'block'
+		},
+		closeModal() {
+			let modal = document.getElementsByClassName('modal')[0]
+			modal.style.display = 'none'
+		},
+	},
+}
 </script>
 
 <style>
 .about {
-  min-height: calc(100vh - 61px - 61px);
+	min-height: calc(100vh - 61px - 61px);
 }
 .fly-in-text {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: row;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: row;
 }
 .fly-in-text h1 {
-  padding: 5px;
-  opacity: 1;
-  transition: all 1.3s ease;
+	padding: 5px;
+	opacity: 1;
+	transition: all 1.3s ease;
 }
 .fly-in-text.hidden h1 {
-  opacity: 0;
+	opacity: 0;
 }
 .fly-in-text.hidden h1:nth-child(1) {
-  transform: translateX(-400px) translateY(-200px);
+	transform: translateX(-400px) translateY(-200px);
 }
 .fly-in-text.hidden h1:nth-child(2) {
-  transform: translateX(20px) translateY(100px);
+	transform: translateX(20px) translateY(100px);
 }
 .fly-in-text.hidden h1:nth-child(3) {
-  transform: translateX(-150px) translateY(-80px);
+	transform: translateX(-150px) translateY(-80px);
 }
 .fly-in-text.hidden h1:nth-child(4) {
-  transform: translateX(10px) translateY(-200px);
+	transform: translateX(10px) translateY(-200px);
 }
 .fly-in-text.hidden h1:nth-child(5) {
-  transform: translateX(-300px) translateY(200px);
+	transform: translateX(-300px) translateY(200px);
 }
 .fly-in-text.hidden h1:nth-child(6) {
-  transform: translateX(20px) translateY(-20px);
+	transform: translateX(20px) translateY(-20px);
 }
 .fly-in-text.hidden h1:nth-child(7) {
-  transform: translateX(50px) translateY(250px);
+	transform: translateX(50px) translateY(250px);
 }
 
 .info-text {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  padding: 0 5% 5px 5%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	text-align: center;
+	padding: 0 5% 5px 5%;
 }
 .title-hobby {
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
 .hobby-object-parent {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-wrap: wrap;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-wrap: wrap;
 }
 
 .hobby-object-under-parent {
-  margin: 1%;
+	margin: 1%;
 }
 .hobby-object-child h3 {
-  height: 5rem;
-  width: 10rem;
-  margin: 1rem 0.25rem 0.25rem 1rem;
-  padding: 0.75rem;
-  border-radius: 6px;
-  background-color: rgb(249, 249, 249);
-  border: 0.65px solid rgb(218, 218, 218);
-  text-align: center;
-  transition-duration: 0.75s;
+	height: 5rem;
+	width: 10rem;
+	margin: 1rem 0.25rem 0.25rem 1rem;
+	padding: 0.75rem;
+	border-radius: 6px;
+	background-color: rgb(255, 255, 255);
+	border: 0.65px solid rgb(218, 218, 218);
+	box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.1);
+	text-align: center;
+	transition-duration: 0.75s;
 }
 .hobby-object-child h3:hover {
-  transform: scale(1.05);
-  transition-duration: 0.75s;
-  cursor: grab;
+	transform: scale(1.05);
+	transition-duration: 0.75s;
+	cursor: grab;
 }
 .hobby-object-child h3:active {
-  cursor: grabbing;
+	cursor: grabbing;
 }
 .projects-object-parent {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
 }
 .project-text {
-  text-align: center;
+	text-align: center;
 }
 .flip-card {
-  background-color: transparent;
-  width: 300px;
-  height: 325px;
-  perspective: 1000px;
-  margin-bottom: 4rem;
+	background-color: transparent;
+	width: 300px;
+	height: 325px;
+	perspective: 1000px;
+	margin-bottom: 4rem;
 }
 
 .flip-card-inner {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  transition: transform 0.8s;
-  transform-style: preserve-3d;
+	position: relative;
+	width: 100%;
+	height: 100%;
+	text-align: center;
+	transition: transform 0.8s;
+	transform-style: preserve-3d;
 }
 
 .flip-card:hover .flip-card-inner {
-  transform: rotateY(180deg);
+	transform: rotateY(180deg);
 }
 
 .flip-card-front,
 .flip-card-back {
-  border-radius: 6px;
-  border: 0.65px solid rgb(218, 218, 218);
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  padding: 0.75rem;
-  -webkit-backface-visibility: hidden;
-  backface-visibility: hidden;
+	border-radius: 6px;
+	border: 0.65px solid rgb(218, 218, 218);
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	padding: 0.75rem;
+	-webkit-backface-visibility: hidden;
+	backface-visibility: hidden;
 }
 
 .flip-card-front {
-  background-color: rgb(249, 249, 249);
-  color: black;
+	background-color: rgb(255, 255, 255);
+	box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.1);
+	color: black;
 }
 
 .flip-card-back {
-  background-color: rgb(249, 249, 249);
-  transform: rotateY(180deg);
-  line-break: strict;
+	background-color: rgb(249, 249, 249);
+	transform: rotateY(180deg);
+	line-break: strict;
 }
 .modal {
-  display: none;
-  position: fixed;
-  z-index: 1;
-  padding-top: 100px;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  overflow: scroll;
-  backdrop-filter: blur(6px);
-  background-color: rgb(0, 0, 0);
-  background-color: rgba(0, 0, 0, 0.6);
+	display: none;
+	position: fixed;
+	z-index: 1;
+	padding-top: 100px;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	overflow: scroll;
+	backdrop-filter: blur(6px);
+	background-color: rgb(0, 0, 0);
+	background-color: rgba(0, 0, 0, 0.6);
 }
 .modal-content {
-  background-color: #fefefe;
-  margin: auto;
-  padding: 20px;
-  border-radius: 6px;
-  width: 80%;
-  text-align: center;
+	background-color: #fefefe;
+	margin: auto;
+	padding: 20px;
+	border-radius: 6px;
+	width: 80%;
+	text-align: center;
 }
 .close {
-  margin-top: -15px;
-  float: right;
-  color: #000000;
-  font-size: 35px;
-  font-weight: bold;
+	margin-top: -15px;
+	float: right;
+	color: #000000;
+	font-size: 35px;
+	font-weight: bold;
 }
 .close:hover {
-  cursor: grab;
+	cursor: grab;
 }
 .close:active {
-  cursor: grabbing;
+	cursor: grabbing;
 }
 @media screen and (max-width: 668px) {
-  .hobby-object-child h3 {
-    width: 7.75rem;
-  }
-  .projects-child {
-    height: 300px;
-    width: 250px;
-  }
-  .flip-card {
-    width: 200px;
-    height: 250px;
-  }
+	.hobby-object-child h3 {
+		width: 7.75rem;
+	}
+	.projects-child {
+		height: 300px;
+		width: 250px;
+	}
+	.flip-card {
+		width: 200px;
+		height: 250px;
+	}
 }
 </style>
