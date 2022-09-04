bf536c93fec9caa6d539fd4593baf668eb6ca832
Matthias Mantsch <matthiasmantsch@MacBook-Pro.local>
2022-09-04 14:51:08 +0200

    main/new(imprint): new info text at imprint

--- src/views/ImprintView.vue
+++ src/views/ImprintView.vue
@@ -1,12 +1,19 @@
 <template>
-	<div class="parent">
+	<div class="parent-imprint">
 		<div class="title">
 			<h1>Imprint</h1>
 			<ol>
 				<li>Owner: Matthias Mantsch</li>
 				<li>Email: matthias@mantsch.ch</li>
-				<li>Host: Vercel</li>
+				<li>Host: <a href="https://vercel.com">Vercel</a></li>
 			</ol>
+		</div>
+		<div class="title">
+			<h1>Info</h1>
+			<p>
+				This Page is only build by me. The only dependency is the framework Vue.js. You can see more of my work on
+				<a href="https://github.com/Blize">Github</a>
+			</p>
 		</div>
 	</div>
 </template>
@@ -16,8 +23,9 @@ export default {}
 </script>
 
 <style>
-.parent {
+.parent-imprint {
 	min-height: calc(100vh - 60px - 61px);
+	padding: 0 30% 0 30%;
 }
 .title {
 	display: flex;
@@ -28,5 +36,14 @@ export default {}
 
 li {
 	list-style: none;
+}
+.title p {
+	text-align: center;
+	margin: 0 3rem 0 3rem;
+}
+@media screen and (max-width: 735px) {
+	.parent-imprint {
+		padding: 0;
+	}
 }
 </style>
