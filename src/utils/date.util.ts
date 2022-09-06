a1006838112c9a80f235acc934e9edc82f57ef2c
Blize <90607489+Blize@users.noreply.github.com>
2022-09-06 16:04:54 +0200

    chore(lint): lint hole project

--- src/utils/date.util.ts
+++ src/utils/date.util.ts
@@ -1,12 +1,8 @@
 export const myDate = (input: string): string => {
-  const date = new Date(input);
-  const year = date.getUTCFullYear();
-  const month =
-    date.getUTCMonth() + 1 >= 10
-      ? date.getUTCMonth() + 1
-      : `0${date.getUTCMonth() + 1}`;
-  const day =
-    date.getUTCDate() >= 10 ? date.getUTCDate() : `0${date.getUTCDate()}`;
+	const date = new Date(input)
+	const year = date.getUTCFullYear()
+	const month = date.getUTCMonth() + 1 >= 10 ? date.getUTCMonth() + 1 : `0${date.getUTCMonth() + 1}`
+	const day = date.getUTCDate() >= 10 ? date.getUTCDate() : `0${date.getUTCDate()}`
 
-  return `${day}.${month}.${year}`;
-};
+	return `${day}.${month}.${year}`
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,109 +1,104 @@
 <template>
-  <div class="curtain">
-    <div class="curtain__wrapper">
-      <input
-        type="checkbox"
-        checked
-        @click="disableInput()"
-        :disabled="validate"
-      />
+	<div class="curtain">
+		<div class="curtain__wrapper">
+			<input type="checkbox" checked @click="disableInput()" :disabled="validate" />
 
-      <div class="curtain__panel curtain__panel--left">
-        <h1>Wel</h1>
-      </div>
+			<div class="curtain__panel curtain__panel--left">
+				<h1>Wel</h1>
+			</div>
 
-      <div class="curtain__content">
-        <h2>Blize</h2>
-      </div>
+			<div class="curtain__content">
+				<h2>Blize</h2>
+			</div>
 
-      <div class="curtain__panel curtain__panel--right">
-        <h1>come</h1>
-      </div>
-    </div>
-  </div>
+			<div class="curtain__panel curtain__panel--right">
+				<h1>come</h1>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "HomeView",
-  data() {
-    return {
-      validate: false,
-    };
-  },
-  methods: {
-    disableInput() {
-      this.validate = true;
-    },
-  },
-});
+	name: 'HomeView',
+	data() {
+		return {
+			validate: false,
+		}
+	},
+	methods: {
+		disableInput() {
+			this.validate = true
+		},
+	},
+})
 </script>
 
 <style lang="scss">
 .curtain {
-  width: 100%;
-  height: calc(100vh -61px -61px);
-  overflow: hidden;
+	width: 100%;
+	height: calc(100vh -61px -61px);
+	overflow: hidden;
 
-  &__wrapper {
-    width: 100%;
-    height: 100%;
+	&__wrapper {
+		width: 100%;
+		height: 100%;
 
-    input[type="checkbox"] {
-      position: absolute;
-      cursor: pointer;
-      width: 99.5%;
-      height: 100%;
-      z-index: 100;
-      opacity: 0;
-      height: calc(100vh -61px -61px);
+		input[type='checkbox'] {
+			position: absolute;
+			cursor: pointer;
+			width: 99.5%;
+			height: 100%;
+			z-index: 100;
+			opacity: 0;
+			height: calc(100vh -61px -61px);
 
-      &:checked {
-        & ~ div.curtain__panel--left {
-          transform: translateX(0);
-        }
+			&:checked {
+				& ~ div.curtain__panel--left {
+					transform: translateX(0);
+				}
 
-        & ~ div.curtain__panel--right {
-          transform: translateX(0);
-        }
-      }
-    }
-  }
+				& ~ div.curtain__panel--right {
+					transform: translateX(0);
+				}
+			}
+		}
+	}
 
-  &__panel {
-    display: flex;
-    align-items: center;
-    background: rgb(250, 250, 250);
-    color: rgb(0, 0, 0);
-    float: left;
-    position: relative;
-    width: 50%;
-    height: 100vh;
-    transition: all 1s ease-out;
-    z-index: 2;
+	&__panel {
+		display: flex;
+		align-items: center;
+		background: rgb(250, 250, 250);
+		color: rgb(0, 0, 0);
+		float: left;
+		position: relative;
+		width: 50%;
+		height: 100vh;
+		transition: all 1s ease-out;
+		z-index: 2;
 
-    &--left {
-      justify-content: flex-end;
-      transform: translateX(-100%);
-    }
-    &--right {
-      justify-content: flex-start;
-      transform: translateX(100%);
-    }
-  }
+		&--left {
+			justify-content: flex-end;
+			transform: translateX(-100%);
+		}
+		&--right {
+			justify-content: flex-start;
+			transform: translateX(100%);
+		}
+	}
 
-  &__content {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: calc(100vh -61px -61px);
-    padding: 1rem 0 0 0;
-    position: absolute;
-    text-align: center;
-    z-index: 1;
-    width: 100%;
-  }
+	&__content {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: calc(100vh -61px -61px);
+		padding: 1rem 0 0 0;
+		position: absolute;
+		text-align: center;
+		z-index: 1;
+		width: 100%;
+	}
 }
 </style>
